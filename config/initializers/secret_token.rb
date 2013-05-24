a74a9d8671fc7109d8f62fbab04413b4b543bcc2
Andrew Seidler <seidler.andrew@gmail.com>
2013-05-23 17:15:53 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keepdom::Application.config.secret_token = '0cc0f7c0292d8571128908ccd3000f8c400afe9a17aee63f9744ac4642a115c18b1fa8b439fe163bde162634f97d56b41362934b2f448c30c043043845ff73e6'
